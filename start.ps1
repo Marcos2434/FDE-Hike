1cc41a97952542c803721a4ad7ce6011f0e6aae3
Marcos Bauch Mira <marcosbauch2434@gmail.com>
2023-11-14 08:25:30 +0100

    fixed start

--- start.ps1
+++ start.ps1
@@ -12,7 +12,7 @@ _PIP_ADDITIONAL_REQUIREMENTS=xlsx2csv==0.7.8 faker==8.12.1
 "@ | Out-File -FilePath $File
 }
 
-docker build -f ./path/to/Dockerfile -t name_of_process ./run_this
+# docker build -f ./path/to/Dockerfile -t name_of_process ./run_this
 
 # Initialize the database
 # On all operating systems, you need to run database migrations and create the first user account.
